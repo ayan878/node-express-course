ef41f6ee196e4b3623636b3104bd82efa462a52c
ayan878 <ayanraza197@gmail.com>
2024-05-01 23:51:54 +0530

    module

---
+++ module.js
@@ -0,0 +1,12 @@
+// Modules
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+const ayan = "ayan";
+const raza = "raza";
+
+const sayHi = (name) => {
+  console.log(`Hello there ${name}`);
+};
+sayHi("Ayan");
+sayHi("ayan");
+sayHi("raza");

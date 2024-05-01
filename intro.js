0413a400aca55d099ff441958d0b8f9ca2840035
ayan878 <ayanraza197@gmail.com>
2024-05-01 23:23:09 +0530

    intro

--- intro.js
+++ intro.js
@@ -5,3 +5,7 @@
 // require  - function to use modules (files)
 // module    - info about current module (file)
 // process   - info about env whrere the program is being excuted
+
+console.log(__dirname); // E:\node-express-course
+
+setInterval(()=>{console.log("Hii Node")},1000)

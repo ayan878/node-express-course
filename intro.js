5daa70af526290f7f180d536ce506d014b2d107e
ayan878 <ayanraza197@gmail.com>
2024-05-01 22:50:04 +0530

    global

---
+++ intro.js
@@ -0,0 +1,7 @@
+// GLOBAL   - NO WINDOW !!!
+
+// __dirname  - path to currrent directory
+// __filename    - file name
+// require  - function to use modules (files)
+// module    - info about current module (file)
+// process   - info about env whrere the program is being excuted

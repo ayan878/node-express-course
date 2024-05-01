9411c308db10a6cae94990864fe686bc2cd342bc
ayan878 <ayanraza197@gmail.com>
2024-05-01 23:59:57 +0530

    intro

---
+++ 1.intro.js
@@ -0,0 +1,13 @@
+// GLOBAL   - NO WINDOW !!!
+
+// __dirname  - path to currrent directory
+// __filename    - file name
+// require  - function to use modules (files)
+// module    - info about current module (file)
+// process   - info about env whrere the program is being excuted
+
+console.log(__dirname); // E:\node-express-course
+
+setInterval(() => {
+  console.log("Hii Node");
+}, 1000);
